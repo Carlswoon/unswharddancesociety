9fe5919aa285b76708de8fdb48fed6ee129b945d
Carlswoon <carlson280306@gmail.com>
2026-05-20 15:01:46 +1000

    fixed bug in home.jsx

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Events from "../components/Events"
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import Events from "../components/Events.jsx"
 function Home() {
   return (
     <main className="bg-black text-white overflow-x-hidden">
